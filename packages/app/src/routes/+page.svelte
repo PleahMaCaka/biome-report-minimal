5208f1029e3553b6768c94bad6686fac89d3b538
PleahMaCaka <pleahmacaka@gmail.com>
2025-08-07 14:59:22 +0900

    remove script

--- packages/app/src/routes/+page.svelte
+++ packages/app/src/routes/+page.svelte
@@ -1,12 +1,3 @@
-<script lang="ts">
-  import { onMount } from "svelte"
-  const a = 1
-
-  onMount(() => {
-    console.log(a)
-  })
-</script>
-
 <svelte:head>
     <title>blabla</title>
 </svelte:head>
